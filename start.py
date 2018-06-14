d77c166fc0d6617a7d45a0268909e8c4c961135e
Hmway Hmway Kyaing <cartoonma.lay01@gmail.com>
2018-06-14 16:14:57 +0630

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+myapp = Flask(__name__)
 
-@app.route("/hmwayhmway")
+@myapp.route("/hmwayhmway")
 def hmwayhmway():
     return "Hello hmwayhmway"
 
-@app.route("/tiide")
+@myapp.route("/tiide")
 def tiide():
     return "Welcome to TIIDE World"
